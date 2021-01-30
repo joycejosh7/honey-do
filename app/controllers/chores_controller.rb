30f58440790965bd573142a0a134095dcfb28756
Joshua <joycejosh7@gmail.com>
2021-01-30 13:56:36 -0500

    added edit, update, and flash errors

--- app/controllers/chores_controller.rb
+++ app/controllers/chores_controller.rb
@@ -7,7 +7,7 @@ class ChoresController < ApplicationController
 
     def show
     end
-
+    
     def new
         @chore = Chore.new
     end
@@ -15,11 +15,28 @@ class ChoresController < ApplicationController
     def create
         @chore = Chore.new(chore_params)
         if @chore.save
+            # what to do if it is valid
             redirect_to chores_path
         else  
+            # what to do if it is invalid
+            flash[:error] = @chore.errors.full_messages
             render :new
         end
     end
+
+    def edit
+            
+    end
+
+    def update
+        if @chore.update(chore_params)
+            redirect_to chore_path(@chore)
+        else  
+            flash[:error] = @chore.errors.full_messages
+            render :edit
+        end
+    end
+
 
 
 private
