d29428e41326c6608b08502d8815e5fd87d6800a
Joshua <joycejosh7@gmail.com>
2021-02-02 15:32:04 -0500

    reorganized nav with helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+    def render_nav_links
+        content_tag("ul") do
+            if true # is logged in
+                content_tag("li")do
+                    link_to("Chores", chores_path)
+                end +
+                content_tag("li") do
+                    link_to("Add Chore", new_chore_path)
+                end
+            else  
+                content_tag("li")do
+                    link_to("Login", "#")
+                end +
+                content_tag("li") do
+                    link_to("Signup", "#")
+                end
+            end
+        end
+    end
 end
